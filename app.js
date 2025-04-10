0de6416884bbaa5d74c7a9cded63b92061417405
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-11 02:11:59 +0530

    Done 7 lecture mind-grenade

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ const { john, peter } = require('./4-names')
 const sayHi = require('./5-utils')
 // const { items, singlePerson } = require('./6-alternative-module')
 const data = require('./6-alternative-module')
+require('./7-mind-bomb');
 
-console.log(data.items);
-console.log(data.singlePerson);
+// console.log(data.items);
+// console.log(data.singlePerson);
 
 // sayHi(john);
 // sayHi(peter);
