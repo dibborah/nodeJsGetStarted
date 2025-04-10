b9bb36f273726580c1c553b787c34ae1d62b8e90
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-11 02:06:14 +0530

    Done the alternative way of exports module: Directly exporting

--- app.js
+++ app.js
@@ -4,8 +4,13 @@
 // Modules -> Encapsulated Code (only share minimum)
 
 const { john, peter } = require('./4-names')
-const sayHi = require('./utils')
+const sayHi = require('./5-utils')
+// const { items, singlePerson } = require('./6-alternative-module')
+const data = require('./6-alternative-module')
 
-sayHi(john);
-sayHi(peter);
-sayHi('Susan');
+console.log(data.items);
+console.log(data.singlePerson);
+
+// sayHi(john);
+// sayHi(peter);
+// sayHi('Susan');
