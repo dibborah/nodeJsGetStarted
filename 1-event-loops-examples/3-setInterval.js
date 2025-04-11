7511facfc692612e4001bc950265c9193f373428
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-12 03:34:50 +0530

    Done: setInterval

---
+++ 1-event-loops-examples/3-setInterval.js
@@ -0,0 +1,9 @@
+setInterval(() => {
+    console.log('hello world');
+  }, 2000);
+  
+  console.log("I will run first");
+  
+  // process stays alive till
+  // kill process CONTROL + C
+  // unexpected errors
