3c2dc1ba53d354404d1b04c7b91343beb826590c
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-11 12:34:42 +0530

    Done path-module

--- 8-os-module.js
+++ 8-os-module.js
@@ -16,4 +16,20 @@
 // sayHi(peter);
 // sayHi('Susan');
 
+const os = require('os');
 
+// Info about the current user
+const user = os.userInfo();
+console.log(user);
+
+// Method returns the system uptime in seconds
+console.log(`The system uptime is ${os.uptime()} seconds`);
+
+const currentOs = {
+    name: os.type(),
+    release: os.release(),
+    totalmem: os.totalmem(),
+    freeMem: os.freemem(),
+}
+
+console.log(currentOs);
